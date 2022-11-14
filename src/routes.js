6148d2b24e0cf766293c4295d09311e475b266ad
Jeff Rynhart <jeff.rynhart@du.edu>
2022-11-14 12:47:34 -0700

    Added 404 route and NotFound page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import Exhibits from './views/Exhibits.svelte';
 import Exhibit from './views/Exhibit.svelte';
+import NotFound from './views/NotFound.svelte';
 
 const routes = [
     {name: '/', component: Exhibits},
     {name: '/exhibits', component: Exhibits},
-    {name: '/exhibit/:id', component: Exhibit}
+    {name: '/exhibit/:id', component: Exhibit},
+    {name: '/404', component: NotFound}
 ]
 
 export { routes }
