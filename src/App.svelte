e9df7900fe68bdefe70ea4a26e4d1b7e72d052b6
Jeff Rynhart <jeff.rynhart@du.edu>
2022-11-29 10:57:41 -0700

    Added global.css file to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,12 @@
 <script>
   import { Router } from 'svelte-router-spa'
   import { routes } from './routes'
-  import { testRoutes } from './test/routes'
   import Header from "./views/partials/Header.svelte";
   import Footer from "./views/partials/Footer.svelte";
-	
 </script>
 
 <Header />
-  <main class="container">
-    <Router {routes} />
-
-    <!-- <Router {testRoutes} /> -->
-  </main>
+<main class="container">
+  <Router {routes} />
+</main>
 <Footer />
-
-<style>
-
-</style>
