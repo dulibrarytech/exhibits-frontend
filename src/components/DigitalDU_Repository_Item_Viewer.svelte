f7f6ae46628a99418ed2af779796f6143935cc52
Jeff Rynhart <jeff.rynhart@du.edu>
2022-12-05 21:12:40 -0700

    Added repository item viewer component

---
+++ src/components/DigitalDU_Repository_Item_Viewer.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { repository } from '../libs/repository';
+    
+    import Image_Viewer from './Image_Viewer.svelte';
+    import Audio_Player from './Audio_Player.svelte';
+    import Video_Player from './Video_Player.svelte';
+    import PDF_Viewer from './PDF_Viewer.svelte';
+    import IiifViewer from './IIIF_Viewer.svelte';
+    
+    export let data;
+
+    //const Repository = new Repository();
+
+    console.log("DigitalDU_Repository_Item: data in:", data)
+
+    // TODO: Fetch item data from repo. Determine if IIIF. If non-iiif, determine media component
+</script>
