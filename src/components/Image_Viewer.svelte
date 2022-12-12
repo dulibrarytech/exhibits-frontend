df73008902804574f3240e2a66f2aff6f40dfba3
Jeff Rynhart <jeff.rynhart@du.edu>
2022-12-12 15:30:02 -0700

    Added openseadragon lib and component, integrated with image viewer component

--- src/components/Image_Viewer.svelte
+++ src/components/Image_Viewer.svelte
@@ -1,24 +1,72 @@
 <script>
-    export let item;
+    import { onMount } from 'svelte';
+    import { Configuration } from '../config/config';
+    import { getItemTypeForFileExtension } from '../libs/media_helpers';
 
-    console.log("Image viewer item in:", item)
+    import OpenSeadragonViewer from './OpenSeadragon_Viewer.svelte';
 
+    export let args;
+
+    console.log("Image viewer data in:", args)
+
+    let type = null;
     let sourceUrl = null;
-    let altText = "";
+    let imageType = null;
+    let altText = "Image";
 
     $: {
-        sourceUrl = item.url || null;
-        altText = item.altText || item.caption || "Image";
+        if(!type) type = args.type || null;
     }
+
+    const render = () => {
+        let {url="", caption=""} = args;
+
+        const urlPattern = /^https?:\/\//;
+        const filenamePattern = /^[\w,\s-]+\.[A-Za-z]{3}$/;
+        
+        if(urlPattern.test(url)) sourceUrl = url;
+        else if(filenamePattern.test(url)) sourceUrl = getImageFilePath(url);
+        else console.error("Image item could not be rendered. Source file or url is invalid or not present");
+
+        if(sourceUrl) {
+            imageType = type || getItemTypeForFileExtension(sourceUrl) || "image";
+            if(!imageType) console.error("Image type could not be determined");
+        }
+
+        if(caption) altText = caption;
+    }
+
+    const getImageFilePath = (filename) => {
+        return "/storage/" + filename; // TODO get path from config*
+    }
+
+    onMount(async () => {
+        render()
+    });
 </script>
 
-<div class="content-container">
+<div class="image-viewer">
     <h6>Image viewer</h6>
-    <div class="image-viewer>">
-        {#if sourceUrl}
-            <img src={sourceUrl} alt={altText} />
+    <div class="image">
+        {#if sourceUrl && imageType}
+            {#if imageType == "image"}
+                <h6>Small image viewer</h6>
+                <img src={sourceUrl} alt={altText} title={altText}/>
+            {:else if imageType == "large_image"}
+                <h6>Large image tile viewer</h6>
+                <OpenSeadragonViewer url={sourceUrl} {altText}/>
+            {:else}
+                <h6>Error</h6>>
+            {/if}
         {:else}
-            <p>Loading image...</p>
+            <h5>Loading image...</h5>
         {/if}
     </div>
 </div>
+
+<style>
+    .image-viewer, .image, img {
+        width: 100%;
+        height: 100%;
+    }
+</style>
---
+++ src/components/OpenSeadragon_Viewer.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { onMount } from 'svelte';
+
+    export let url;
+    export let altText = "Image";
+
+    var viewer = null;
+
+    onMount(async () => {
+        try {
+            viewer = OpenSeadragon({
+                id: "openseadragon1",
+                prefixUrl: "/libs/openseadragon/images/",
+                tileSources: url
+            });
+        }
+        catch(e) {
+            console.error(`Error initializing OpenSeadragon viewer: ${e}`);
+        }
+
+        console.log("Init OSD:", viewer)
+    });
+</script>
+
+<div id="openseadragon1" style="width: 800px; height: 600px;" title={altText}></div>
+
+<style>
+
+</style>
