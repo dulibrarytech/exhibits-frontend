f249f339b7f176ef4bfadc2a6929cf23bb31ec67
Jeff Rynhart <jeff.rynhart@du.edu>
2022-12-14 15:11:47 -0700

    Added audio and video player component base

--- src/components/Audio_Player.svelte
+++ src/components/Audio_Player.svelte
@@ -1,8 +1,32 @@
 <script>
-    export let item;
-    export let sourceUrl=null; // If null, use url from item data. If present, use this.
+    import Kaltura_Content from "./Kaltura_Content.svelte";
+    import Embed_Code_Content from "./Embed_Code_Content.svelte";
+    //import JWPlayer_Content from "./JWPlayer_Content.svelte";
 
-    console.log("Audio player item in:", item)
+    export let args = {};
+
+    var kalturaId = null;
+    var embedCode = null;
+    var url = null;
+
+    console.log("Audio player data in:", args)
+
+    const render = () => {
+
+    }
 </script>
 
-<h6>Audio player</h6>
+<div class="audio-player">
+    <h6>Audio player</h6>
+    <div class="audio content">
+        {#if kalturaId}
+            <Kaltura_Content id={kalturaId} />
+        {:else if embedCode}
+            <Embed_Code_Content code={embedCode} />
+        {:else if url}
+            <audio src={url}></audio>
+        {:else}
+            <h6>Loading audio content...</h6>
+        {/if}
+    </div>
+</div>
--- src/components/Video_Player.svelte
+++ src/components/Video_Player.svelte
@@ -1,7 +1,35 @@
 <script>
-    export let item;
+    import Kaltura_Content from "./Kaltura_Content.svelte";
+    import Embed_Code_Content from "./Embed_Code_Content.svelte";
+    //import JWPlayer_Content from "./JWPlayer_Content.svelte";
 
-    console.log("Video player item in:", item)
+    export let args = {};
+
+    var kalturaId = null;
+    var embedCode = null;
+    var url = null;
+
+    console.log("Video player data in:", args)
+
+    const render = () => {
+
+    }
 </script>
 
-<h6>Video player</h6>
+<div class="video-player">
+    <h6>Video player</h6>
+    <div class="video content">
+        {#if kalturaId}
+            <Kaltura_Content id={kalturaId} />
+        {:else if embedCode}
+            <Embed_Code_Content code={embedCode} />
+        {:else if url}
+            <video controls>
+                <track kind="captions">
+                <source src={url} type="video/mp4"> <!-- get src ext from url? -->
+            </video>
+        {:else}
+            <h6>Loading video content...</h6>
+        {/if}
+    </div>
+</div>
