e6ee06fae94e8fe5db589af189bd57649d2e969b
Jeff Rynhart <jeff.rynhart@du.edu>
2023-02-16 08:54:24 -0700

    Added kaltura interface and viewer component

--- src/components/Kaltura_Content.svelte
+++ src/components/Kaltura_Content.svelte
@@ -1,25 +1,28 @@
 <script>
+    import { Settings } from '../config/settings';
+    import { Kaltura } from '../libs/kaltura';
+
     export let id = null;
     export let caption = "Untitled content";
-    export let width = "600";
-    export let height = "400";
 
-    // TODO: to settings
-    const KALTURA_DOMAIN = "https://cdnapisec.kaltura.com";
-    const PARTNER_ID = "2357732";
-    const UI_ID = "44058172";
-    const UNIQUE_OBJECT_ID = "kaltura_player_1559751114";
+    var {   
+        kalturaUniqueObjectID,
+        kalturaPlayerHeight,
+        kalturaPlayerWidth
+
+    } = Settings;
 
     var kalturaUrl = null;
 
     $: {
-        if(id && !kalturaUrl) kalturaUrl = `${KALTURA_DOMAIN}/p/${PARTNER_ID}00/embedIframeJs/uiconf_id/${UI_ID}/partner_id/${PARTNER_ID}?iframeembed=true&playerId=${UNIQUE_OBJECT_ID}&entry_id=${id}&flashvars[leadWithHTML5]=true`;
+        if(id) kalturaUrl = Kaltura.getEmbeddedViewerUrl(id);
+        else console.error("Missing Kaltura entry id");
     }
 </script>
 
 <div class="kaltura content">
     {#if kalturaUrl}
-        <iframe id={UNIQUE_OBJECT_ID} title={caption} src={kalturaUrl} {width} {height} allowfullscreen webkitallowfullscreen mozAllowFullScreen allow='autoplay *; fullscreen *; encrypted-media *' frameborder='0'></iframe> 
+        <iframe id={kalturaUniqueObjectID} title={caption} src={kalturaUrl} width={kalturaPlayerWidth} height={kalturaPlayerHeight} allowfullscreen webkitallowfullscreen mozAllowFullScreen allow='autoplay *; fullscreen *; encrypted-media *' frameborder='0'></iframe> 
     {:else}
         <h5>Loading Kaltura content...</h5>
     {/if}
---
+++ src/libs/kaltura.js
@@ -0,0 +1,78 @@
+/**
+ Copyright 2023 University of Denver
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/**
+* Kaltura
+*/
+
+'use strict'
+
+import { Settings } from '../config/settings';
+
+export const Kaltura = (() => {
+
+    var {   
+        kalturaDomain,
+        kalturaPartnerID,
+        kalturaUI_ID,
+        kalturaUniqueObjectID,
+        kalturaThumbnailWidth,
+        kalturaThumbnailHeight,
+        kalturaFlavorId
+
+    } = Settings;
+
+    /**
+     * 
+     * @method getViewerContent
+     * 
+     * @param {string} entryId 
+     * 
+     * @returns 
+     */
+    const getEmbeddedViewerUrl = async (entryId) => {
+        return `${kalturaDomain}/p/${kalturaPartnerID}/sp/${kalturaPartnerID}00/embedIframeJs/uiconf_id/${kalturaUI_ID}/partner_id/${kalturaPartnerID}?iframeembed=true&playerId=${kalturaUniqueObjectID}&entry_id=${entryId}&flashvars[leadWithHTML5]=true`;
+    }
+
+    /**
+     * @method getThumbnailUrl
+     * 
+     * @param {string} entryId 
+     * 
+     * @returns 
+     */
+    const getThumbnailUrl = async (entryId) => {
+        return `${kalturaDomain}/p/${kalturaPartnerID}/thumbnail/entry_id/${entryId}/width/${kalturaThumbnailWidth}/height/${kalturaThumbnailHeight}`;
+    }
+
+    /**
+     * 
+     * @method getStreamingMediaUrl
+     * 
+     * @param {string} entryId
+     * 
+     * @returns 
+     */
+    const getStreamingMediaUrl = async (entryId) => {
+        return `${kalturaDomain}/p/${kalturaPartnerID}/sp/0/playManifest/entryId/${entryId}/format/url/protocol/https/flavorParamId/${kalturaFlavorId}`;
+    }
+
+    return {
+        getEmbeddedViewerUrl,
+        getThumbnailUrl,
+        getStreamingMediaUrl
+    };
+})()
