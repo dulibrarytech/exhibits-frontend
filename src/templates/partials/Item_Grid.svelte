fe7b9c0a97b11e3d3b37bcff73bcf077a419caee
Jeff Rynhart <jeff.rynhart@du.edu>
2023-04-20 15:37:28 -0600

    Applied bootstrap grid template to Item Grid items

--- src/templates/partials/Item_Grid.svelte
+++ src/templates/partials/Item_Grid.svelte
@@ -1,18 +1,27 @@
 <script>
+    /* Requires bootstrap > 5.2.3 */
     'use strict'
     
     import Grid_Item_Image_Text from './Grid_Item_Image_Text.svelte';
 
-    // Shows title as heading
-
     export let items = [];
     export let columns = 4; 
+
+    console.log("TEST GRID", (12 % 1))
+
+    const BOOTSTRAP_GRID_SIZE = "lg" // sm, md, lg;
+
+    var columnValue = "3";
+
+    $: {
+        columnValue = 12 / columns;
+    }
 </script>
 
 <div class="item-grid">
     {#each items as item}
         {#if item.is_published}
-            <div class="item item-col-{columns}">
+            <div class="item col-{BOOTSTRAP_GRID_SIZE}-{columnValue}">
                 <!-- select grid item component from available -->
                 <Grid_Item_Image_Text {item} />
                 <!-- look in other item props to determine type of grid item: text (normal), overlay text, wrap text -->
@@ -30,23 +39,5 @@
 
     .item {
         padding: 5px;
-    }
-
-    /* 4 col */ 
-    .item-col-4 {
-        min-width: 22%;
-        width: 22%;
-    }
-
-    /* 3 col */ 
-    .item-col-3 {
-        min-width: 30%;
-        width: 30%;
-    }
-
-    /* 2 col */ 
-    .item-col-2 {
-        min-width: 47%;
-        width: 47%;
     }
 </style>
