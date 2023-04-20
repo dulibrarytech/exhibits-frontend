e8a972e711c6aed9abd5a59ab7e64b99ec69eaca
Jeff Rynhart <jeff.rynhart@du.edu>
2023-04-19 19:23:31 -0600

    Added favicon, du logo, and applied bootstrap template to existing du header

--- src/views/partials/Header.svelte
+++ src/views/partials/Header.svelte
@@ -1,3 +1,18 @@
-<div class="top-header">
-    <p>Header</p>
+<div class="du-header w-100 header-logo-wrapper">
+    <div class="header-logo">
+        <a href="https://library.du.edu" title="University Libraries" target="_blank"><img src="../assets/images/du-logo2.gif" alt="University of Denver Logo" /></a>
+    </div>
 </div>
+
+<style>
+    .du-header {
+        height: 50px;
+        background: #8b2332;
+    }
+
+    .du-header .header-logo {
+        position: relative;
+        top: 4px;
+        left: 5px;
+    }
+</style>
