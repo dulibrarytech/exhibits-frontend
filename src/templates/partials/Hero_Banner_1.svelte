eb3b77677bc4533cab0689d9ff4a2564daaecaea
Jeff Rynhart <jeff.rynhart@du.edu>
2023-04-26 18:36:51 -0600

    Applied hero template for text overlay

--- src/templates/partials/Hero_Banner_1.svelte
+++ src/templates/partials/Hero_Banner_1.svelte
@@ -19,30 +19,32 @@
 
     onMount(async () => {
         dispatch('mount', {});
+        
+        document.querySelector('.hero-image').style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("${image}")`;
     });
 </script>
 
 <header class="banner">
-    <img src={image} alt={title}/>
-    <div class="title-text">
-        <div id="title" class="overlay-text text">{title}</div>
-        <hr>
-        {#if subtitle}<div id="subtitle" class="overlay-text text">{subtitle}</div>{/if}
+    <div class="hero-image">
+        <div class="hero-text">
+            <div id="title" class="overlay-text text">{title}</div>
+            <hr>
+            {#if subtitle}<div id="subtitle" class="overlay-text text">{subtitle}</div>{/if}
+        </div>
     </div>
     
-</header>
-
-{#if description}
-    <section class="introduction">
-        <div class="container px-4">
-            <div class="row gx-4 justify-content-center">
-                <div class="col-lg-8">
-                    <p>{@html description}</p>
+    {#if description}
+        <section class="introduction">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-8">
+                        <p>{@html description}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-{/if}
+        </section>
+    {/if}
+</header>
 
 <style>
     header {
@@ -55,6 +57,8 @@
 
     .banner {
         position: relative;
+        /* min-height: 50%; */
+        height: 50%
     }
 
     .introduction {
@@ -63,10 +67,22 @@
         padding-bottom: 75px;
     }
 
-    .title-text {
+    .hero-image {
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        position: relative;
+        min-height: 700px;
+        /* height: 100%; */
+    }
+
+    .hero-text {
+        text-align: center;
         position: absolute;
-        top: 0;
-        padding: 30px;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: white;
     }
 
     #title {
@@ -94,10 +110,6 @@
 
         #subtitle {
             font-size: 1.3em;
-        }
-
-        .title-text {
-            padding: 45px 80px;
         }
     }
 
