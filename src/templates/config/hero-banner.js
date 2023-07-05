f7b14f5e8ec5f8f431162abf71a5476fb9f587db
Jeff Rynhart <jeff.rynhart@du.edu>
2023-07-05 09:49:52 -0600

    Added banner template with no title text

--- src/templates/config/hero-banner.js
+++ src/templates/config/hero-banner.js
@@ -5,11 +5,13 @@ import { writable } from 'svelte/store';
  ************************************************/
 import Hero_Banner_1 from '../exhibit/hero/partial/Hero_Banner_1.svelte';
 import Hero_Banner_2 from '../exhibit/hero/partial/Hero_Banner_2.svelte';
+import Hero_Banner_3 from '../exhibit/hero/partial/Hero_Banner_3.svelte';
 
 /**********************************************
  * {component id} : {component}
  **********************************************/
 export const Banners = writable({
     "banner_1": Hero_Banner_1,
-    "banner_2": Hero_Banner_2
+    "banner_2": Hero_Banner_2,
+    "banner_3": Hero_Banner_3
 })
---
+++ src/templates/exhibit/hero/partial/Hero_Banner_3.svelte
@@ -0,0 +1,74 @@
+<script>
+    'use strict'
+    /*
+     * Hero_Banner_2
+     * hero template
+     *
+     * fullwidth hero image
+     * fullwidth title and descriptive text section 
+     */
+    import { onMount } from 'svelte';
+    import {createEventDispatcher} from 'svelte';
+
+    export let args = {};
+
+    let {image=null, title="exhibit title", subtitle=null, description=null} = args;
+
+    const dispatch = createEventDispatcher();
+
+    onMount(async () => {
+        dispatch('mount', {});
+        
+        document.querySelector('.hero-image').style.backgroundImage = `url("${image}")`;
+    });
+</script>
+
+<div class="banner">
+    <section class="hero-image"></section>
+    
+    <section class="hero-text">
+        <div class="container">
+            {#if description}
+                <div class="description-text">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <p>{@html description}</p>
+                        </div>
+                    </div>
+                </div>
+            {/if}
+        </div>
+    </section>
+</div>
+
+<style>
+    header {
+        padding: 0;
+    }
+
+    .banner {
+        position: relative;
+        height: 50%
+    }
+
+    .hero-image {
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        position: relative;
+        min-height: 700px;
+    }
+
+    .hero-text {
+        /* text-align: center; */
+        color: white;
+        /* padding: 3.75em 0; */
+        padding-top: 50px;
+        padding-bottom: 50px;
+    }
+
+    .description-text {
+        margin-top: 2.25em;
+        margin-bottom: 2.25em;
+    }
+</style>
