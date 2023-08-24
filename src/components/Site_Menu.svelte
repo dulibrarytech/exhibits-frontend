ab0d32e2621bbfaade31aadde01a1217b2935076
Jeff Rynhart <jeff.rynhart@du.edu>
2023-08-24 15:18:37 -0600

    Added collapsing site menu component, and menu configuration to define links

--- src/components/Site_Menu.svelte
+++ src/components/Site_Menu.svelte
@@ -1,14 +1,22 @@
 <script>
+    /* 
+     * Requires bootstrap >=5.0
+     */
     'use strict'
 
     import { onMount } from 'svelte';
-    import jQuery from 'jquery';
+    //import jQuery from 'jquery';
+    import { Menu_Links } from '../config/menu.js';
 
-    jQuery.noConflict(true)
+    let links = null;
 
-    console.log("jQuery:", jQuery('.site-link'))
+    //jQuery.noConflict(true)
 
-    jQuery('#testid').hide()
+    //console.log("jQuery:", jQuery('.site-link'))
+
+    //jQuery('#testid').hide()
+
+    $: links = Menu_Links;
 
     onMount(() => {
 		//window.jQuery = jQuery;
@@ -16,23 +24,62 @@
         //console.log("window jQuery:", jQuery)
 
         //siteSticky();
+        
+        // iterate Menu, adding links to links[]
+        // OR links = Menu_Links
 	});
 </script>
 
-<div class="main-menu" id="testid">
-    <a href="#"><i class="bi bi-list"></i></a>
+<div class="site-menu-toggle">
+    <a data-bs-toggle="collapse" href="#menu" role="button" aria-expanded="false" aria-controls="menu">
+        <i class="bi bi-list"></i>
+    </a>
+</div>
+
+<div class="collapse site-menu" id="menu">
+    <div class="site-menu-links card card-body">
+        {#each links as link}
+            {#if link.open_new_tab}
+                <a href={link.url} target="_blank">{link.label}</a>
+            {:else}
+                <a href={link.url}>{link.label}</a>
+            {/if}
+        {/each}
+    </div>
 </div>
 
 <style>
-    .main-menu {
+    .site-menu-toggle {
         position: absolute;
         right: 9px;
         top: -3px;
         z-index: 10;
     }
 
-    .main-menu i {
+    .site-menu-toggle i {
         font-size: 2.2em;
         color: white;
+    }
+
+    .site-menu {
+        background: rgb(229, 227, 225);
+    }
+
+    .site-menu .card {
+        border: none;
+        border-radius: unset;
+        flex-direction: row;
+    }
+
+    .site-menu a {
+        color: #616161;
+    }
+
+    .site-menu a:not(:first-child) {
+        margin-left: 20px;
+    }
+
+    .site-menu-links {
+        background: inherit;
     }
 </style>
---
+++ src/config/menu.js
@@ -0,0 +1,26 @@
+/**
+ * Site menu configuration
+ */
+
+export const Menu_Links = [
+    {
+        "label": "What's On",
+        "url": "/whats-on",
+        "open_new_tab": false
+    },
+    {
+        "label": "Events",
+        "url": "/events",
+        "open_new_tab": false
+    },
+    {
+        "label": "Student Curated",
+        "url": "/student-curated",
+        "open_new_tab": false
+    },
+    {
+        "label": "About Us",
+        "url": "https://library.du.edu/collections-library-materials/exhibits",
+        "open_new_tab": true
+    }
+]
