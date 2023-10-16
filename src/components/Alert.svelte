3803b84eaf7fc9f30066efa201814c79c30d6be9
Jeff Rynhart <jeff.rynhart@du.edu>
2023-10-16 10:58:21 -0600

    Added Alert component

---
+++ src/components/Alert.svelte
@@ -0,0 +1,20 @@
+<script>
+    'use strict'
+
+    export let text = "";
+</script>
+
+<div class="alert-section">
+    <div class="outer-container">
+        {@html text}
+    </div>
+</div>
+
+<style>
+    .alert-section {
+        min-height: 4.2em;
+        padding: 1.3em 1.6em;
+        margin-bottom: 0;
+        background-color: lightgoldenrodyellow;
+    }
+</style>
