e5bd35c2ccdbe78df30e9beabf11e7f155a63efa
Jeff Rynhart <jeff.rynhart@du.edu>
2023-11-27 14:56:18 -0700

    Moved the 'visit' on-hover overlay from the exhibit preview component up one level into the exhibit preview grid component

--- src/components/Exhibit_Preview.svelte
+++ src/components/Exhibit_Preview.svelte
@@ -9,7 +9,6 @@
     export let height = null;
 
     let titleTextElement;
-    let overlayDisplay;
 
     let exhibitPath = null;
     let thumbnail;
@@ -35,7 +34,6 @@
         if(!height) height = EXHIBIT_THUMBNAIL_HEIGHT;
         
         if(!thumbnail) {
-            
             thumbnail = Resource.getImageDerivativeUrl({
                 type: 'crop',
                 filename: heroImage || "",
@@ -47,24 +45,10 @@
     }
 
     const setTheme = () => {
-        let {image = {}} = styles;
-
-        /* Set the title text color to match the color of the exhibit hero image text */
-        if(image.color) {
-            titleTextElement.style.color = image.color;
+        let exhibit_hero_image = styles.image || {};
+        for(let style in exhibit_hero_image) {
+            titleTextElement.style[style] = exhibit_hero_image[style];
         }
-    }
-
-    const mouseOver = (e) => {
-        overlayDisplay = "block";
-    }
-
-    const mouseOut = (e) => {
-        overlayDisplay = "none";
-    }
-
-    const setOverlayDisplay = (isVisible=true) => {
-        overlayDisplay = isVisible ? "block" : "none";
     }
 
     onMount(async () => {
@@ -73,7 +57,7 @@
     });
 </script>
 
-<div class="exhibit-preview" on:mouseenter={mouseOver} on:mouseleave={mouseOut}>
+<div class="exhibit-preview">
     {#if exhibitPath}
         <a href="{exhibitPath}" bind:this={titleTextElement}>
             <div class="exhibit-thumbnail">
@@ -87,10 +71,6 @@
         </a>
     {/if}
 </div>
-
-<div class="overlay" style="display:{overlayDisplay}"></div>
-
-<div class="overlay-text" style="display:{overlayDisplay}">VISIT</div>
 
 <style>
     .exhibit-preview a {
@@ -112,32 +92,5 @@
         height: 100%;
         position: relative;
         border-radius: 5px;
-    }
-
-    .overlay {
-        display: none;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: 10;
-        background-color: #181818;
-        opacity: 0.4;
-        pointer-events: none;
-    }
-
-    .overlay-text {
-        display: none;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: calc(50% - 13px);
-        left: 0;
-        z-index: 11;
-        color: white;
-        font-size: 24px;
-        text-align: center;
-        pointer-events: none;
     }
 </style>
--- src/templates/Exhibit_Preview_Grid.svelte
+++ src/templates/Exhibit_Preview_Grid.svelte
@@ -20,8 +20,11 @@
 
     {#if previews.length > 0}
         {#each previews as exhibit}
-            <div class="grid-item col-lg-3">
+            <div class="grid-item exhibit-preview-item col-lg-3">
                 <Exhibit_Preview {exhibit} />
+
+                <div class="overlay"></div>
+                <div class="overlay-text">VISIT</div>
             </div>
         {/each}
     {/if}
@@ -29,6 +32,10 @@
 </div>
 
 <style>
+    .col-lg-3 {
+        width: 23.5%;
+    } 
+
     .exhibits-preview-grid {
         display: flex;
         flex-wrap: wrap;
@@ -41,8 +48,36 @@
     .grid-item {
         position: relative;
     }
+    
+    .grid-item:hover .overlay,
+    .grid-item:hover .overlay-text {
+        display: block;
+    }
 
-    .col-lg-3 {
-        width: 23.5%;
+    .overlay {
+        display: none;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 10;
+        background-color: #181818;
+        opacity: 0.4;
+        pointer-events: none;
+    }
+
+    .overlay-text {
+        display: none;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: calc(50% - 13px);
+        left: 0;
+        z-index: 11;
+        color: white;
+        font-size: 24px;
+        text-align: center;
+        pointer-events: none;
     }
 </style>
