e9362af4f424d34f010890d69cd6db62bf06994e
Jeff Rynhart <jeff.rynhart@du.edu>
2023-12-04 13:26:54 -0700

    Moved item title div external to the item content div to position it above the media item, and external to the main item content

--- src/templates/partials/Item.svelte
+++ src/templates/partials/Item.svelte
@@ -61,7 +61,6 @@
 
         if(titleElement) {
             titleElement.style.fontFamily = heading.fontFamily || 'inherit';
-            //titleElement.style.color = heading.color || 'inherit';
         }
     }
 
@@ -80,6 +79,9 @@
     <div id={id ?? undefined} class="anchor-offset"></div>
 
     <div class="container">
+        {#if title && showTitle}
+            <div class="title-heading" bind:this={titleElement}>{title}</div><br>
+        {/if}
 
         {#if layout == MEDIA_POSITION.RIGHT}
             {#if wrapText}
@@ -90,9 +92,9 @@
                         </a>
                         {#if caption}<div class="caption">{caption}</div>{/if}
                     </div>
-                    {#if title && showTitle}<div class="title-heading" bind:this={titleElement}>{title}</div><br>{/if}
                     <Text_Display {item} />
                 </div>
+
             {:else}
                 <div class="item-content media-right">
                     <div class="media" style="width:{mediaWidth}%">
@@ -102,14 +104,15 @@
                         {#if caption}<div class="caption">{caption}</div>{/if}
                     </div>
                     <div class="text" style="width:{100 - mediaWidth}%">
-                        {#if title && showTitle}<div class="title-heading" bind:this={titleElement}>{title}</div><br>{/if}
                         <Text_Display {item} />
                     </div>
                 </div>
+
             {/if}
 
         {:else if layout == MEDIA_POSITION.LEFT}
             {#if wrapText}
+
                 <div class="item-content wrap-text text media-left">
                     <div class="media" style="width:{mediaWidth}%">
                         <a href data-item-id={uuid} on:click|stopPropagation|preventDefault={onClickItem}>
@@ -117,7 +120,6 @@
                         </a>
                         {#if caption}<div class="caption">{caption}</div>{/if}
                     </div>
-                    {#if title && showTitle}<div class="title-heading" bind:this={titleElement}>{title}</div><br>{/if}
                     <Text_Display {item} />
                 </div>
             {:else}
@@ -129,15 +131,14 @@
                         {#if caption}<div class="caption">{caption}</div>{/if}
                     </div>
                     <div class="text" style="width:{100 - mediaWidth}%">
-                        {#if title && showTitle}<div class="title-heading" bind:this={titleElement}>{title}</div><br>{/if}
                         <Text_Display {item} />
                     </div>
                 </div>
+
             {/if}
             
         {:else if layout == MEDIA_POSITION.TOP}
             <div class="item-content media-top">
-                {#if title && showTitle}<div class="title-heading" bind:this={titleElement}>{title}</div><br>{/if}
                 <div class="media media-fullwidth" style="width:{mediaWidth}%">
                     <a href data-item-id={uuid} on:click|stopPropagation|preventDefault={onClickItem}>
                         <Media_Display {item} args={{showPreview: true}} />
@@ -151,7 +152,6 @@
 
         {:else if layout == MEDIA_POSITION.BOTTOM}
             <div class="item-content media-bottom">
-                {#if title && showTitle}<div class="title-heading" bind:this={titleElement}>{title}</div><br>{/if}
                 <div class="text">
                     <Text_Display {item} />
                 </div>
@@ -165,7 +165,6 @@
 
         {:else if layout == MEDIA_POSITION.MEDIA_ONLY}
             <div class="item-content">
-                {#if title && showTitle}<div class="title-heading" bind:this={titleElement}>{title}</div><br>{/if}
                 <div class="media media-fullwidth" style="width:{mediaWidth}%">
                     <a href data-item-id={uuid} on:click|stopPropagation|preventDefault={onClickItem}>
                         <Media_Display {item} args={{showPreview: true}} />
@@ -176,12 +175,10 @@
 
         {:else if layout == MEDIA_POSITION.TEXT_ONLY}
             <div class="item-content text">
-                {#if title && showTitle}<div class="title-heading" bind:this={titleElement}>{title}</div><br>{/if}
                 <Text_Display {item} />
             </div>
 
         {/if}
-
     </div>
 </div>
 
@@ -228,27 +225,12 @@
         float: none;
     }
 
-    /* .media-left.wrap-text > .media {
-        padding: 0 50px 50px 0;
-    }
-
-    .media-right.wrap-text > .media {
-        padding: 0 0 50px 50px;
-    } */
-
-    /* .media-left:not(.wrap-text) > .media,
-    .media-left:not(.wrap-text) > .text,
-    .media-right:not(.wrap-text) > .media,
-    .media-right:not(.wrap-text) > .text {
-        height: 100%;
-    } */
-
     .media-top > .media {
-        padding: 0 0 30px 0;
+        padding: 0 0 50px 0;
     }
 
     .media-bottom > .media {
-        padding: 30px 0 0 0;
+        padding: 50px 0 0 0;
     }
 
     .caption {
@@ -256,7 +238,6 @@
         text-decoration: none;
         color: inherit;
         font-style: italic;
-        /* font-size: 0.85em; */
     }
 
     :global(.item .caption) {
@@ -285,7 +266,6 @@
     @media (min-width:768px) {
         .media-right > .media {
             float: right;
-            /* padding: 0 0 30px 30px; */
         }
 
         .media-right > .text {
@@ -294,7 +274,6 @@
 
         .media-left > .media {
             float: left;
-            /* padding: 0 30px 30px 0; */
         }
 
         .media-left > .text {
