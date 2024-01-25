d741d12b841ba429917ff1760c2edec220a8eb59
Jeff Rynhart <jeff.rynhart@du.edu>
2024-01-25 14:23:45 -0700

    Added client cache module

---
+++ src/libs/cache.js
@@ -0,0 +1,20 @@
+import lscache from 'lscache';
+
+export const Cache = (() => {
+
+    const storeExhibits = (exhibits) => {
+        lscache.set('exhibits', exhibits);
+    }
+
+    const getExhibitById = (exhibitId) => {
+        let exhibits = lscache.get('exhibits');
+        return exhibits.find((exhibit) => {
+            return exhibit.uuid == exhibitId;
+        });
+    }
+
+    return {
+        storeExhibits,
+        getExhibitById
+    }
+})()
