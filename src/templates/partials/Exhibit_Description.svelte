dcf0bd5f50f4eb9ab827a8bffdb690c8aadc9dd5
Jeff Rynhart <jeff.rynhart@du.edu>
2024-08-27 16:50:20 -0600

    removed container margin from description section, added side padding, updated responsive padding and font size

--- src/templates/partials/Exhibit_Description.svelte
+++ src/templates/partials/Exhibit_Description.svelte
@@ -2,16 +2,16 @@
     export let content = "";
 </script>
 
-<div class="exhibit-description container">
+<div class="exhibit-description">
     {@html content}
 </div>
 
 <style>
     .exhibit-description {
         height: 100%;
-        padding: 50px 0;
-        font-size: 1.2em;
+        font-size: 1.0em;
         line-height: 1.45em;
+        padding: 1.5em 1.5em;
     }
 
     :global(.exhibit-description p) {
@@ -22,22 +22,28 @@
     @media screen and (min-width: 480px) {
         .exhibit-description {
             font-size: 1.0em;
+            padding: 2.5em 3.5em;
         }
     }
 
     @media screen and (min-width: 768px) {
-
+        .exhibit-description {
+            font-size: 1.1em;
+            padding: 3.5em 4.5em;
+        }
     }
 
     @media screen and (min-width: 992px) {
         .exhibit-description {
-            font-size: 1.1em;
+            font-size: 1.15em;
+            padding: 3.5em 5.5em;
         }
     }
 
     @media screen and (min-width: 1280px) {
         .exhibit-description {
             font-size: 1.2em;
+            padding: 3.5em 6.5em;
         }
     }
 </style>
