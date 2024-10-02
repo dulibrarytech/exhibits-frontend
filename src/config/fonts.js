253ebc7cf3484daac78fba8b6759d8d9bd3ff292
Jeff Rynhart <jeff.rynhart@du.edu>
2024-10-02 16:31:39 -0600

    added NeueHaasUnicaW1G font

--- src/config/fonts.js
+++ src/config/fonts.js
@@ -3,10 +3,10 @@ export const Fonts = [
         "name": "Nocturne Serif",
         "file": "NocturneSerifTest-Medium-uploaded-63b633de32d04.otf",
         "url": ""
+    },
+    {
+        "name": "Neue Haas Unica W1G",
+        "file": "NeueHaasUnicaW1G-Black.otf",
+        "url": "" 
     }
-    // {
-    //     "name": "Neue Haas Unica W1G",
-    //     "file": "",
-    //     "url": "" 
-    // }
 ]
