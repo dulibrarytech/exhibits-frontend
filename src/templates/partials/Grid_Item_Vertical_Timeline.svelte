8cbd8987c19961ed856aff2d40f0887cf467fe95
Jeff Rynhart <jeff.rynhart@du.edu>
2024-10-07 16:41:19 -0600

    removed 'vertical timeline 2' template and moved code to vertical timeline template

---
+++ src/templates/partials/Grid_Item_Vertical_Timeline.svelte
@@ -0,0 +1,192 @@
+<script>
+    'use-strict'
+
+    import { onMount } from 'svelte';
+    import Item_Preview from '../../components/Media_Item_Preview.svelte';
+
+    export let item = {};
+
+    let id = null;
+    let styles = null;
+
+    let itemElement;
+
+    let type;
+    let date;
+    let title;
+    let caption;
+    let description;
+    let media;
+
+    $: {
+        id = item.uuid || "null";
+        date = item.date || null;
+        title = item.title || null;
+        caption = item.caption || null;
+        description = item.description || null;
+        type = item.item_type || null;
+        media = item.media || null;
+
+        if(title) title = title.toUpperCase();
+
+        // TODO move to map function in parent grid (grid items map)
+        try {
+            styles = JSON.parse(item.styles) || {};
+        }
+        catch(error) {
+            console.error(`Error loading item styles: ${error}; uuid: ${id}`);
+        }
+    }
+
+    const setTheme = ({item = {}}) => {
+        Object.assign(itemElement.style, item)
+    }
+
+    onMount(() => {
+        if(styles) setTheme(styles); 
+    });
+</script>
+
+<div class="vertical-timeline-grid-item item" {id}>
+    
+    <div class="timeline__card card" bind:this={itemElement}>
+        <header class="card__header">
+            <!-- <time class="time" datetime="2008-02-02">
+                <span class="time__day">2</span>
+                <span class="time__month">Feb</span>
+            </time> -->
+
+            <!-- {#if date}
+                <div class="time">{date}</div>
+            {:else}
+                <div class="time">N.D.</div>
+            {/if} -->
+
+            {#if title}<div class="card__title"><strong>{@html title}</strong></div><hr>{/if}
+
+        </header>
+
+        <div class="card__content">
+            {#if description && description.length > 0}<p class="description">{description}</p>{/if}
+
+            {#if media} 
+                <div class="preview">
+                    <Item_Preview {item} on:click-item />
+                    {#if caption}<div class="caption">{caption}</div>{/if}
+                </div>
+            {/if}
+        </div>
+    </div>
+</div>
+
+<style>
+    /*
+    =====
+    DEPENDENCES
+    =====
+    */
+
+    .r-title{
+        margin-top: var(--rTitleMarginTop, 0) !important;
+        margin-bottom: var(--rTitleMarginBottom, 0) !important;
+    }
+    
+    
+    p:not([class]){
+        line-height: var(--cssTypographyLineHeight, 1.78);
+        margin-top: var(--cssTypographyBasicMargin, 1em);
+        margin-bottom: 0;
+    }
+    
+    p:not([class]):first-child{
+        margin-top: 0;
+    }
+
+    /*
+    core styles
+    */
+
+    .time{
+        display: var(--timeDisplay, inline-flex);
+    }
+    
+    /*
+    extensions
+    */
+    
+    .time__month{
+        margin-left: var(--timelineMounthMarginLeft, .25em);
+    }
+    
+    /*
+    skin
+    */
+    
+    .time{
+        /* padding: var(--timePadding, .25rem 1.25rem .25rem);
+        background-color: var(--timeBackgroundColor, #f0f0f0); */
+        font-size: var(--timeFontSize, 1.24rem);
+        font-weight: var(--timeFontWeight, 700);
+        text-transform: var(--timeTextTransform, uppercase);
+        color: var(--timeColor, currentColor);
+    }
+
+    .card{
+        padding: var(--timelineCardPadding, 1.5rem 1.5rem 1.25rem);
+    }
+    
+    .card__content{
+        /* margin-top: var(--cardContentMarginTop, .5rem); */
+        margin: 0 auto 2rem auto;
+        width: 90%;
+    }
+
+    .card__content > a {
+        text-decoration: none;
+        color: inherit;
+    }
+    
+    /*
+    skin
+    */
+    
+    .card{
+        /* box-shadow: var(--timelineCardBoxShadow, 0 1px 3px 0 rgba(0, 0, 0, .12), 0 1px 2px 0 rgba(0, 0, 0, .24)); */
+        background-color: var(--timelineCardBackgroundColor, #fff);
+    }
+    
+    /*
+    extensions
+    */
+    
+    .card__title{
+        --rTitleMarginTop: var(--cardTitleMarginTop, 1rem);
+        font-size: var(--cardTitleFontSize, 1.25rem);
+    }
+
+    /*
+    text component
+    */
+    
+    .text{
+        display: var(--textDisplay, inline-flex);
+        font-size: var(--textFontSize, 1rem);  
+    }
+
+    /* DU updates */
+    .preview {
+        margin-top: 30px;
+    }
+
+    p.description {
+        margin-top: 1rem;
+    }
+
+    .caption {
+        margin-top: 1rem;
+        text-decoration: none;
+        color: inherit;
+        font-style: italic;
+        font-size: 0.8em;
+    }
+</style>
