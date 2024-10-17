cdb9e4829ce270066a371fcda5b8540366e5a474
Jeff Rynhart <jeff.rynhart@du.edu>
2024-10-17 09:47:59 -0600

    removed test metadata object from iiif item test module, placed it into external test file
    ref #44555ca7f2df30104195908f9f0b935d520bd07e

--- src/test/components/IIIF_Item_test.svelte
+++ src/test/components/IIIF_Item_test.svelte
@@ -8,35 +8,34 @@
     //     },
     //     item: {} // exhibit item
     // }
+    // test_metadata = {
+    //     "title": "Test Title",
+    //     "uri": "{handle}",
+    //     "identifiers": [
+    //         {
+    //             "type": "local",
+    //             "identifier": "M123.45.00006"
+    //         }
+    //     ],
+    //     "resource_type": "sound recording nonmusical",
+    //     "dates": [
+    //         {
+    //             "encoding": "w3cdtf",
+    //             "label": "creation",
+    //             "type": "single",
+    //             "expression": "1999 August 5",
+    //             "begin": "1999-08-05"
+    //         },
+    //         {
+    //             "encoding": "w3cdtf",
+    //             "label": "creation",
+    //             "type": "single",
+    //             "expression": "2003",
+    //             "begin": "2003"
+    //         }
+    //     ]
+    // }
     import {test_data} from '../data/iiif-item.js';
-
-    export const test_metadata = {
-        "title": "Test Title",
-        "uri": "/repositories/2/archival_objects/123456",
-        "identifiers": [
-            {
-                "type": "local",
-                "identifier": "M123.45.00006"
-            }
-        ],
-        "resource_type": "sound recording nonmusical",
-        "dates": [
-            {
-                "encoding": "w3cdtf",
-                "label": "creation",
-                "type": "single",
-                "expression": "1999 August 5",
-                "begin": "1999-08-05"
-            },
-            {
-                "encoding": "w3cdtf",
-                "label": "creation",
-                "type": "single",
-                "expression": "2003",
-                "begin": "2003"
-            }
-        ]
-    }
 
     const TEST = "E";
 
