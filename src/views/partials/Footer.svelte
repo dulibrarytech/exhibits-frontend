3f9cfc6003310d8ebdd90f20e299c77c0c87f592
Jeff Rynhart <jeff.rynhart@du.edu>
2024-11-13 11:14:33 -0700

    added copyright and app version to site footer

--- src/views/partials/Footer.svelte
+++ src/views/partials/Footer.svelte
@@ -2,9 +2,13 @@
     <div class="container">
         <div class="footer-content row">
             <div class="col-lg-12">
-                <p style="font-weight: bolder">
-                    Exhibits @ DU
-                </p>
+
+                <p style="font-weight: bolder;">Exhibits @ DU</p>
+
+                <p style="font-size: 0.8em;">Version 1.0.0 beta</p>
+
+                <p style="font-size: 0.8em;">© 2024 University of Denver</p>
+                
             </div>
         </div>
     </div>
