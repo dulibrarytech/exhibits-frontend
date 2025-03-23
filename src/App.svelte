2d1ecf3f67d6c49ab757a9e82c93b2a4774174be
Jeff Rynhart <jeff.rynhart@du.edu>
2025-03-23 13:47:41 -0600

    added site branding template to app template under the site menu

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,14 @@
 
   import Header from "./views/partials/Header.svelte";
   import Footer from "./views/partials/Footer.svelte";
+  import Site_Branding from './templates/partials/Site_Branding.svelte';
 </script>
 
 <Header />
 <main class="container-1700">
+  <Site_Branding />
+
+  <!-- render page -->
   <Router {routes} />
 </main>
 <Footer />
---
+++ src/templates/partials/Site_Branding.svelte
@@ -0,0 +1,48 @@
+<svelte:head>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+</svelte:head>
+
+<script>
+    'use strict'
+</script>
+
+<div class="site-branding">
+    <div class="branding-text">
+        <a class="site-title" href="/">University Libraries
+            <h1>Exhibits <span class="highlight-text">@ DU</span></h1>
+        </a>
+    </div>
+</div>
+
+<style>
+    .site-branding {
+        height: 100px;
+        width: 100%;
+        display: table;
+    }
+
+    .branding-text {
+        display: table-cell;
+        vertical-align: middle;
+    }
+
+    a.site-title {
+        font-size: 18px;
+        font-family: "Montserrat";
+        display: block;
+        color: #333333;
+        text-decoration: none;
+        position: relative;
+        left: 30px;
+    }
+
+    .site-title h1 {
+        font-size: 27px;
+        text-transform: uppercase;
+    }
+
+    .highlight-text {
+        color: #8b2332;
+        border-color: #8b2332;
+    }
+</style>
