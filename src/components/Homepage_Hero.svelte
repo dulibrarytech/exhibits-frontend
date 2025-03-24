7deca26dda5ef6c2fd846f8bb3d9a3dc4f3992d3
Jeff Rynhart <jeff.rynhart@du.edu>
2025-03-24 16:19:04 -0600

    added hero section base template to new home page

---
+++ src/components/Homepage_Hero.svelte
@@ -0,0 +1,41 @@
+<script>
+    'use strict'
+
+    // this might be a template
+
+    import { onMount } from 'svelte';
+    import { Settings } from '../config/settings.js';
+
+    export let exhibit;
+
+    let heroElement;
+
+    onMount(async () => {
+        //heroElement.style.background = "url('/assets/images/AAC-exhibits.jpg')";
+    })
+</script>
+
+<div class="homepage-hero" bind:this={heroElement}>
+    <div class="container">
+        <div class="row">
+
+            <!-- highlight exhibit title, description -->
+            <div class="col-sm-12 col-md-6">
+
+            </div>
+
+            <!-- highlight exhibit preview -->
+            <div class="col-sm-12 col-md-6">
+
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<style>
+    .homepage-hero {
+        height: 400px;
+        background-color: lightgrey;
+    }
+</style>
