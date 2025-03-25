cb6ea53bdd73d09b1649e7da7105208514a01662
Jeff Rynhart <jeff.rynhart@du.edu>
2025-03-25 13:40:51 -0600

    Added toggle for hero background image vs hero highlight item template

--- src/components/Homepage_Hero.svelte
+++ src/components/Homepage_Hero.svelte
@@ -8,34 +8,48 @@
 
     export let exhibit;
 
+    const LAYOUT = {
+        BACKGROUND_IMAGE: 'background_image',
+        HIGHLIGHT_ITEM: 'highlight_item'
+    }
+
+    const HERO_LAYOUT = LAYOUT.HIGHLIGHT_ITEM;
+    const BACKGROUND_IMAGE_PATH = "/assets/images/AAC-exhibits.jpg";
+
     let heroElement;
 
     onMount(async () => {
-        //heroElement.style.background = "url('/assets/images/AAC-exhibits.jpg')";
+        if(HERO_LAYOUT == LAYOUT.BACKGROUND_IMAGE) {
+            heroElement.style.background = `url('${BACKGROUND_IMAGE_PATH}')`;
+        }
     })
 </script>
 
 <div class="homepage-hero" bind:this={heroElement}>
-    <div class="container">
-        <div class="row">
 
-            <!-- highlight exhibit title, description -->
-            <div class="col-sm-12 col-md-6">
+    {#if HERO_LAYOUT == LAYOUT.HIGHLIGHT_ITEM}
+        <div class="container">
+            <div class="row">
+
+                <!-- highlight exhibit title, description -->
+                <div class="col-sm-12 col-md-6">
+
+                </div>
+
+                <!-- highlight exhibit preview -->
+                <div class="col-sm-12 col-md-6">
+
+                </div>
 
             </div>
-
-            <!-- highlight exhibit preview -->
-            <div class="col-sm-12 col-md-6">
-
-            </div>
-
         </div>
-    </div>
+    {/if}
 </div>
 
 <style>
     .homepage-hero {
         height: 400px;
-        background-color: lightgrey;
+        background-color: #e5e3e1;
+        opacity: 0.6;
     }
 </style>
