a95c38064aabaff42563c3dbf0f26d1b79d92e1c
Jeff Rynhart <jeff.rynhart@du.edu>
2025-04-08 17:42:20 -0600

    updated sitewide default fomt to Arial

--- src/App.svelte
+++ src/App.svelte
@@ -4,26 +4,21 @@
 
   import Header from "./views/partials/Header.svelte";
   import Footer from "./views/partials/Footer.svelte";
-  // import Site_Branding from './templates/partials/Site_Branding.svelte';
 </script>
 
 <main class="container-1700">
   <Header />
-  <!-- <Site_Branding /> -->
 
   <!-- render page -->
   <Router {routes} />
+  
   <Footer />
 </main>
 
 <style>
-  @font-face {
-        font-family: "Breve Sans Text";
-        src: url('../assets/fonts/BreveSansTextLight.otf') format("truetype");
-  }
 
   main {
-    font-family: "Breve Sans Text";
+    font-family: "Arial";
     min-height: calc(100vh - 249px); 
   }
 </style>
