5e0ada95998277021708b42a3606fbe5aa06d30a
Jeff Rynhart <jeff.rynhart@du.edu>
2025-04-10 14:37:13 -0600

    removed 'about us' link from site menu

--- src/config/menu.js
+++ src/config/menu.js
@@ -12,10 +12,5 @@ export const Menu_Links = [
         "label": "All Exhibits",
         "url": "/exhibits-explore",
         "open_new_tab": false
-    },
-    {
-        "label": "About Us",
-        "url": "https://library.du.edu/collections-library-materials/exhibits",
-        "open_new_tab": true
     }
 ]
