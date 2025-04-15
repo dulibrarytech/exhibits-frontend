fd2d2d8c838707ce05208ddc5da7bf0733c50418
Jeff Rynhart <jeff.rynhart@du.edu>
2025-04-15 17:51:46 -0600

    added min page height with footer visible to 404 page

--- src/views/NotFound.svelte
+++ src/views/NotFound.svelte
@@ -1,3 +1,10 @@
 <div class="container" style="margin-top: 50px">
     <h3 style="color: black">Page Not Found</h3>
 </div>
+
+<style>
+    .container {
+        margin-top: 50px;
+        min-height: calc(100vh - 349px);
+    }
+</style>
