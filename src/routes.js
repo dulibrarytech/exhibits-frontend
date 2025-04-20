4928a26a8ff769ba46f0ae665406c21f5a984d7c
Jeff Rynhart <jeff.rynhart@du.edu>
2025-04-20 14:02:10 -0600

    removed 'student curated'page and old home page removed 'student curated' page from routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-//import Home_Page from './views/Home_Page.svelte';
 import Exhibit from './views/Exhibit.svelte';
 import Search from './views/Search.svelte';
 import Whats_On from './views/Whats_On.svelte';
 import Events from './views/Events.svelte';
-import Student_Curated from './views/Student_Curated.svelte';
 
 import Test_Component from './test/Test_Component.svelte';
 import Test_View from './test/Test_View.svelte';
@@ -45,7 +43,6 @@ const routes = [
      **********************/
     {name: '/whats-on', component: Whats_On},
     {name: '/events', component: Events},
-    {name: '/student-curated', component: Student_Curated},
 
 
     /**********************
