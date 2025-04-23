bedd66727791f427b9db8532503551f8207bdb2a
Jeff Rynhart <jeff.rynhart@du.edu>
2025-04-23 16:43:45 -0600

    removed 'download exhibit' link from exhibit menu

--- src/components/Exhibit_Menu.svelte
+++ src/components/Exhibit_Menu.svelte
@@ -21,7 +21,7 @@
         <div class="d-flex flex-column flex-md-row align-items-left py-2">
             <nav class="d-flex align-items-center text-dark text-decoration-underline">
                 <a href class="me-3 py-2 text-dark text-decoration-underline" data-page-id="about-the-curators" on:click|preventDefault={onClickLink} >About the Curators</a>
-                <a href class="me-3 py-2 text-dark text-decoration-underline" data-page-id="download-exhibit" on:click|preventDefault={onClickLink} >Download Exhibit</a>
+                <!-- <a href class="me-3 py-2 text-dark text-decoration-underline" data-page-id="download-exhibit" on:click|preventDefault={onClickLink} >Download Exhibit</a> -->
             </nav>
         
             <div class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
