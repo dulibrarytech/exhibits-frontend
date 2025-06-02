4dee72c3fcdabb469d93599c95dbff74133f1c0e
Jeff Rynhart <jeff.rynhart@du.edu>
2025-06-02 14:38:52 -0600

    added 'return to home page ' link to not found (404) page

--- src/views/NotFound.svelte
+++ src/views/NotFound.svelte
@@ -1,10 +1,16 @@
 <div class="container" style="margin-top: 50px">
     <h3 style="color: black">Page not found</h3>
+    <br>
+    <a href="/" title="Link to home page">Return to Exhibits @ DU home page</a>
 </div>
 
 <style>
     .container {
         margin-top: 50px;
         min-height: calc(100vh - 349px);
+    }
+
+    a {
+        text-decoration: underline;
     }
 </style>
