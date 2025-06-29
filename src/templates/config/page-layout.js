8f4f11b8c5ba54aa0d5035106cd2e8e59b3d6839
Jeff Rynhart <jeff.rynhart@du.edu>
2025-06-29 11:52:09 -0600

    removed exhibit side navigation page from project

--- src/templates/config/page-layout.js
+++ src/templates/config/page-layout.js
@@ -4,12 +4,10 @@ import { writable } from 'svelte/store';
  * exhibit page layout template components
  **********************************************/
 import Top_Nav from '../Exhibit_Page_Top_Nav.svelte';
-import Side_Nav from '../Exhibit_Page_Side_Nav.svelte';
 
 /**********************************************
  * {component id} : {component}
  **********************************************/
 export const Page_Layouts = writable({
     "top_nav": Top_Nav,
-    "side_nav": Side_Nav
 })
