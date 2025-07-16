4235ccabb8ec7470235da9e2c7d465e983b04891
Jeff Rynhart <jeff.rynhart@du.edu>
2025-07-16 14:15:22 -0600

    updated repository related items test component

--- src/test/components/Repository_Related_Items_test.svelte
+++ src/test/components/Repository_Related_Items_test.svelte
@@ -1,8 +1,10 @@
 <script>
     import Repository_Related_Items from '../../components/Repository_Related_Items.svelte';
     import {test_data} from '../data/related-repo-items.js';
+
+    console.log(test_data)
 </script>
 
 <div class="test">
-    <Repository_Related_Items {test_data} />
+    <Repository_Related_Items items={test_data} />
 </div>
