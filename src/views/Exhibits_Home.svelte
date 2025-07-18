dulibrarytech/exhibits-frontend

commit 9af60a169f1699ee90b1ca42637a79342d23ec64

added 'view more' link to homepage heading sections to open the 'all exhibits' page

src/views/Exhibits_Home.svelte
@@ -104,6 +104,8 @@
                         <div class="homepage-section">
                             <div class="heading">
                                 <h1>Featured Exhibits</h1>
+
+                                <a href="/exhibits-explore">View More</a>
                             </div>
 
                             {#if EXHIBITS_DISPLAY == "slider"}
@@ -118,6 +120,8 @@
                         <div class="homepage-section">
                             <div class="heading">
                                 <h1>Student Curated Exhibits</h1>
+
+                                <a href="/exhibits-explore">View More</a>
                             </div>
 
                             {#if EXHIBITS_DISPLAY == "slider"}
@@ -169,5 +173,34 @@
     .homepage-section {
         margin-top: 50px;
         margin-bottom: 50px;
+    }
+
+    .homepage-section .heading {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: baseline;
+        margin-bottom: 50px;
+        row-gap: 20px;
+    }
+
+    .homepage-section .heading h1 {
+        margin-bottom: 0;
+    }
+
+    .homepage-section .heading a {
+        text-decoration: underline;
+        color: #71190E;
+        font-family: "IBM Plex Mono";
+    }
+
+    :global(.homepage-section .exhibit-preview-grid) {
+        justify-content: space-between !important;
+    }
+
+    @media screen and (min-width: 480px) {
+        .homepage-section .heading {
+            flex-direction: row;
+        }
     }
 </style>

commit 9c759bef270c3593af953452d5a876155fea40d1

updated recent homepage section flex grid update to be scoped to home page (fixes update from being applied to exhibits explore page)
issue caused by commit 9af60a169f1699ee90b1ca42637a79342d23ec64

src/views/Exhibits_Home.svelte
@@ -194,7 +194,7 @@
         font-family: "IBM Plex Mono";
     }
 
-    :global(.homepage-section .exhibit-preview-grid) {
+    :global(.exhibits-home .homepage-section .exhibit-preview-grid) {
         justify-content: space-between !important;
     }
 