6c1f98cd5b197f22ef1e85d1042ec4c6f2f83905
Jeff Rynhart <jeff.rynhart@du.edu>
2025-08-18 09:57:59 -0600

    added updated site branding banner with added search box (forget to add in previous commit)
    ref #5f1c8e751122b747074c9b5f233b9235427f33f1
    
    github #90

---
+++ src/templates/partials/Site_Branding_Search.svelte
@@ -0,0 +1,80 @@
+<svelte:head>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+</svelte:head>
+
+<script>
+    import { Settings } from '../../config/settings.js';
+    import Search_Box from '../../components/Search_Box.svelte';
+
+    export let args = {};
+
+    let _searchFields = Object.keys(Settings.searchFieldsExhibitItem);
+</script>
+
+<div class="site-branding-banner">
+    <div class="container d-flex flex-column flex-md-row align-items-left py-2 site-branding">
+
+        <div class="d-flex align-items-center site-title-link">
+            <a href="/" class="me-3 py-2">
+                <p>Exhibits <span class="highlight-text">@ DU</span></p>
+            </a>
+        </div>
+    
+        <div class="d-inline-flex mt-2 mt-md-0 ms-md-auto search-form">
+            <Search_Box endpoint="/search" fields={_searchFields} placeholder="Search exhibits"/>
+        </div>
+        
+    </div>
+</div>
+
+<style>
+    .site-branding-banner {
+        border-bottom-style: solid;
+        border-bottom-width: 1px;
+        border-bottom-color: #e5e3e1;
+    }
+    
+    .site-branding {
+        height: 100px;
+        width: 100%;
+        display: table;
+    }
+
+    .site-title-link {
+        display: table-cell;
+        vertical-align: middle;
+        position: relative;
+        left: -2px;
+    }
+
+    .site-title-link a {
+        width: fit-content;
+        font-size: 18px;
+        font-family: "Montserrat";
+        display: block;
+        color: #333333;
+        text-decoration: none;
+    }
+
+    .site-title-link p {
+        font-size: 33px;
+        text-transform: uppercase;
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+
+    .highlight-text {
+        color: #BA0C2F;
+        border-color: #BA0C2F;
+    }
+
+    .search-form {
+        width: unset;
+    }
+
+    @media screen and (min-width: 992px) {
+        .search-form {
+            width: 400px;
+        }
+    }
+</style>
