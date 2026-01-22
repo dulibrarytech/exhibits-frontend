1f2d8f70d67dfb6896c959188c63c44298354eae
Jeff Rynhart <jeff.rynhart@du.edu>
2026-01-22 10:48:18 -0700

    updated bootstrap file remote server pings to fix https related errors

--- exhibits-client.js
+++ exhibits-client.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const compress = require('compression');
 const axios = require('axios');
+const https = require('https');
 const Logger = require('./log4.js');
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 if (process.env.NODE_ENV === 'production') {
   app.use(compress());
@@ -27,26 +28,30 @@ app.use(function(req, res, next) {
 
 app.get('*', (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
-})
+});
 
-// test image server
-axios.get(process.env.EXHIBITS_IIIF_IMAGE_SERVER_URL, {rejectUnauthorized: false})
-  .then(function (response) {
-    Logger.module().info(`Image server is online at '${process.env.EXHIBITS_IIIF_IMAGE_SERVER_URL}'`);
-  })
-  .catch(function (error) {
-    Logger.module().error(`Image server is unavailable at '${process.env.EXHIBITS_IIIF_IMAGE_SERVER_URL}' ${error}`);
-  });
+const agent = new https.Agent({
+    rejectUnauthorized: false,
+});
 
-// test exhibits server
-axios.get(process.env.EXHIBITS_API_DOMAIN, {rejectUnauthorized: false})
-  .then(function (response) {
-    Logger.module().info(`Exhibits server is online at '${process.env.EXHIBITS_API_DOMAIN}'`);
-  })
-  .catch(function (error) {
-    Logger.module().error(`Exhibits server is unavailable at '${process.env.EXHIBITS_API_DOMAIN}' ${error}`);
-  });
+// test image server availability
+axios.get(process.env.EXHIBITS_IIIF_IMAGE_SERVER_URL, {httpsAgent: agent})
+    .then(function (response) {
+        Logger.module().info(`Image server is online at '${process.env.EXHIBITS_IIIF_IMAGE_SERVER_URL}'`);
+    })
+    .catch(function (error) {
+        Logger.module().error(`Image server is unavailable at '${process.env.EXHIBITS_IIIF_IMAGE_SERVER_URL}' ${error}`);
+    });
+
+// test exhibits server availability
+axios.get(process.env.EXHIBITS_API_DOMAIN, {httpsAgent: agent})
+    .then(function (response) {
+        Logger.module().info(`Exhibits server is online at '${process.env.EXHIBITS_API_DOMAIN}'`);
+    })
+    .catch(function (error) {
+        Logger.module().error(`Exhibits server is unavailable at '${process.env.EXHIBITS_API_DOMAIN}' ${error}`);
+    });
 
 app.listen(process.env.NODE_PORT || 5000, () => {
-  Logger.module().info(`Exhibits frontend is running on port ${process.env.NODE_PORT} in ${process.env.NODE_ENV} mode`);
-})
+    Logger.module().info(`Exhibits frontend is running on port ${process.env.NODE_PORT} in ${process.env.NODE_ENV} mode`);
+});
