a1953abdd74ab64579033ccaf4dc416905df4bcf
Jeff Rynhart <jeff.rynhart@du.edu>
2026-04-20 09:49:20 -0600

    added new homepage hero file (*from move /components -> /templates/partials)
    ref commit #6e9c362b886f30ed9773e8c0dea6da801a635edd

---
+++ src/templates/partials/Homepage_Hero.svelte
@@ -0,0 +1,54 @@
+<script>
+    'use strict'
+
+    import { onMount } from 'svelte';
+    import { Settings } from '../../config/settings.js';
+
+    const LAYOUT = {
+        BACKGROUND_IMAGE: 'background_image',
+        HIGHLIGHT_ITEM: 'highlight_item'
+    }
+
+    const HERO_LAYOUT = LAYOUT.HIGHLIGHT_ITEM;
+
+    let {   
+        homepageHeroImage,
+    } = Settings;
+
+    let _heroElement;
+
+    onMount(async () => {
+        if(HERO_LAYOUT == LAYOUT.BACKGROUND_IMAGE) {
+            _heroElement.style.background = `url('${homepageHeroImage}') no-repeat center center`;
+        }
+    })
+</script>
+
+<div class="homepage-hero" bind:this={_heroElement}>
+
+    {#if HERO_LAYOUT == LAYOUT.HIGHLIGHT_ITEM}
+        <div class="container">
+            <div class="row">
+
+                <!-- highlight exhibit title, description -->
+                <div class="col-sm-12 col-md-6">
+
+                </div>
+
+                <!-- highlight exhibit preview -->
+                <div class="col-sm-12 col-md-6">
+
+                </div>
+
+            </div>
+        </div>
+    {/if}
+</div>
+
+<style>
+    .homepage-hero {
+        height: 400px;
+        background-color: #e5e3e1;
+        opacity: 0.6;
+    }
+</style>
