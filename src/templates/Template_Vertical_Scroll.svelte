6915b8d98ef741abe5876d8220fac2aacdb8cc17
Jeff Rynhart <jeff.rynhart@du.edu>
2026-06-09 13:44:18 -0600

    added 'pad top' attribute to exhibit subheading element in vertical scroll template (fixes missing update from subheadings branch merge)

--- src/templates/Template_Vertical_Scroll.svelte
+++ src/templates/Template_Vertical_Scroll.svelte
@@ -80,7 +80,7 @@
 
                      <!-- exhibit heading -->
                     {:else if type == ENTITY_TYPE.EXHIBIT_SUBHEADING} 
-                        <Exhibit_Subheading id={anchorId} {text} styles={displayItems[index].styles || null} display={is_visible} on:mount-template-item={onMountTemplateItem} />
+                        <Exhibit_Subheading id={anchorId} {text} styles={displayItems[index].styles || null} display={is_visible} padTop={index > 0 && displayItems[index - 1].type == ENTITY_TYPE.EXHIBIT_HEADING} on:mount-template-item={onMountTemplateItem} />
                     
                         <!-- exhibit item container - grid -->
                     {:else if type == ENTITY_TYPE.GRID}
