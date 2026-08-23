79ba87d52811b7c1e6cf215fd85cf1640c254508
Jeff Rynhart <jeff.rynhart@du.edu>
2026-08-23 14:39:06 -0600

    updated search module querystring value assignment to desructure pattern updated search module variables to use underscore prefix removed the 'cache' boolean url parameter and functionality (automated testing of cache to determine presence of selected facets on page load)

--- src/views/Search.svelte
+++ src/views/Search.svelte
@@ -13,53 +13,54 @@
     import { Settings } from '../config/settings.js';
     import { Cache } from '../libs/cache';
     import * as Logger from '../libs/logger.js';
-
     import Search_Results_Display from '../components/Search_Results_Display.svelte';
 
     import {
-        ENTITY_TYPE, 
         INDEX_FIELD, 
         SEARCH_BOOLEAN, 
         SEARCH_TYPE
     } from '../config/global-constants.js';
 
     const RESULTS_PER_PAGE = Settings.searchResultsPerPage || 10;
+    const DEFAULT_SEARCH_FIELD = INDEX_FIELD.TITLE;
+    const DEFAULT_SEARCH_OPERATOR = SEARCH_BOOLEAN.AND;
 
     export let currentRoute;
 
-    var results = null;
-    var limitOptions = null;
-    var facets = [];
-    var searchParams = {};
-    var message = null;
+    // module variables
+    let _results = null;
+    let _limitOptions = null;
+    let _facets = [];
+    let _searchParams = {};
+    let _message = null;
 
-    let terms;
-    let boolean;
-    let fields;
-    let entity;
-    let page;
-    let cache;
-    let exhibitId;
+    // search query values
+    let {
+        q:          terms = "",
+        fields:     fields = DEFAULT_SEARCH_FIELD,
+        bool:       boolean = DEFAULT_SEARCH_OPERATOR,
+        page:       page = 1,
+        exhibitId:  exhibitId = null,
+
+    } = currentRoute.queryParams || {};
 
     const init = async () => {
-        message = "Searching...";
+        _message = "Searching...";
 
-        terms = currentRoute.queryParams.q?.split(',') || "";
-        fields = currentRoute.queryParams.fields?.split(',') || INDEX_FIELD.TITLE;
-        boolean = currentRoute.queryParams.bool || SEARCH_BOOLEAN.AND;
-        page = currentRoute.queryParams.page || 1;
-        entity = currentRoute.queryParams.index || ENTITY_TYPE.EXHIBIT;
-        cache = currentRoute.queryParams.cache || false;
-        exhibitId = currentRoute.queryParams.exhibitId || null;
+        // convert terms, fields data to arrays
+        terms = terms.split(',');
+        fields = fields.split(',');
 
-        searchParams = {
+        // init search params fields
+        _searchParams = {
             searchType: exhibitId ? SEARCH_TYPE.SEARCH_EXHIBIT : SEARCH_TYPE.SEARCH_ALL,
             pageNumber: page,
             resultsPerPage: RESULTS_PER_PAGE,
             totalResults: 0
         }
 
-        if(cache) facets = Cache.getSearchData()?.selectedFacets || [];
+        // check if there are any selected facets
+        _facets = Cache.getSearchData()?.selectedFacets || [];
 
         let response = false;
         if(validateUrlParameters()) {
@@ -69,16 +70,16 @@
             Logger.module().error("Search error: Invalid query params");
         }
         
-        message = response == true ? null : message = "An error occurred when executing the search.";
+        _message = response == true ? null : "An error occurred when executing the search.";
     }
 
     const executeSearch = async () => {
         try {
-            let response = await Search.execute({terms, boolean, fields, exhibitId, facets});
+            let response = await Search.execute({terms, boolean, fields, exhibitId, facets: _facets});
             
-            results = response.results || [];
-            limitOptions = response.limitOptions || null;
-            searchParams.totalResults = response.resultCount || null;
+            _results = response.results || [];
+            _limitOptions = response.limitOptions || null;
+            _searchParams.totalResults = response.resultCount || null;
 
             return true;
         }
@@ -94,9 +95,6 @@
         // boolean must be global value
         if(boolean && Object.values(SEARCH_BOOLEAN).includes(boolean) === false) isValid = false;
 
-        // entity must be global value 
-        if(entity && Object.values(ENTITY_TYPE).includes(entity) === false) isValid = false;
-
         // id must be hex value
         if(exhibitId && /^[a-fA-F0-9\-]+$/g.test(exhibitId) === false) isValid = false;
 
@@ -107,41 +105,27 @@
     }
 
     const onSelectFacet = (event) => {
-        facets = event.detail;
-        Cache.storeSearchData({selectedFacets: facets});
+        _facets = event.detail;
+        Cache.storeSearchData({selectedFacets: _facets});
 
+        // revert to results page 1 when selected facets are updated
         let url = window.location.href;
-        if(!cache) {
-            url += "&cache=true";
-            cache = true;
-        }
-
         window.location.replace( url.replace(/(&|)page=[0-9]+/g, "") );
     } 
 
     const onRemoveFacet = (event) => {
-        facets = event.detail;
-        Cache.storeSearchData({selectedFacets: facets});
-            
-        let url = window.location.href;
-        if(!cache) {
-            url += "&cache=true";
-            cache = true;
-        }
+        _facets = event.detail;
+        Cache.storeSearchData({selectedFacets: _facets});
 
+        // revert to results page 1 when selected facets are updated
+        let url = window.location.href;
         window.location.replace( url.replace(/(&|)page=[0-9]+/g, "") );
     }
 
     const onResetFacets = (event) => {
-        facets = [];
+        _facets = [];
         Cache.deleteSearchData();
-
-        let url = window.location.href;
-        if(cache) {
-            url = url.replace("&cache=true", "");
-            cache = false;
-        }
-        window.location.replace(url);
+        window.location.reload();
     }
 
     const onClickBack = (event) => {
@@ -157,14 +141,14 @@
 
 <div class="search-page page">
     <div class="search-results container-large">
-        {#if results}
+        {#if _results}
 
             <Search_Results_Display 
-                {results} 
-                {facets} 
-                {limitOptions} 
-                {terms}
-                {searchParams}
+                results={_results} 
+                facets={_facets} 
+                limitOptions={_limitOptions} 
+                terms={terms}
+                searchParams={_searchParams}
 
                 on:click-facet={onSelectFacet} 
                 on:click-clear-facets={onResetFacets} 
@@ -174,9 +158,9 @@
             />
 
         {:else}
-            {#if message}
+            {#if _message}
                 <div class="message">
-                    <h5>{message}</h5>
+                    <h5>{_message}</h5>
                 </div>        
             {/if}
         {/if}
