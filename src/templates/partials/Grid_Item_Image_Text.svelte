0ba6d726e888fe430ba08ba7a6854446b2ab76ca
Jeff Rynhart <jeff.rynhart@du.edu>
2026-09-13 15:55:52 -0600

    replaced media item preview component with Item template in standard grid items
    github #170

--- src/templates/partials/Grid_Item_Image_Text.svelte
+++ src/templates/partials/Grid_Item_Image_Text.svelte
@@ -8,8 +8,7 @@
     'use strict'
 
     import { onMount } from 'svelte';
-    //import Item from './Item.svelte';
-    import Item_Preview from '../../components/Media_Item_Preview.svelte';
+    import Item from '../../templates/partials/Item.svelte';
     import Item_Display from '../../components/Item_Display.svelte';
 
     import { MEDIA_POSITION, ITEM_TYPE } from '../../config/global-constants';
@@ -62,18 +61,20 @@
     {/if}
 
     {#if title}<h4>{@html title}</h4>{/if}
+
     <Item_Display 
         {item} 
         
-        template={Item_Preview} 
+        template={Item} 
         args={{
-            // isThumbnail: true,
             verifyThumbnail: true,
+            showPreview: true,
             showTitle: true, 
             gridItem: true,
         }} 
         on:click-item 
     />
+
 </div>
 
 <style>
